85728b82c5c7ad2234be9479ba38b32a21811363
Ugur Yagmur <cuy.official@gmail.com>
2024-03-02 17:36:10 +0000

    multiple object rendering

--- ray_tracing/main.cpp
+++ ray_tracing/main.cpp
@@ -1,32 +1,17 @@
+#include "utility.h"
+
 #include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "sphere.h"
+#include "hittable_list.h"
 
 #include <iostream>
 
-double hit_sphere(const point3& center, double radius, const ray& r)
+
+color rayColor(const ray& r, const hittable& world)
 {
-	vec3 oc = r.origin() - center;
-	auto a = dot(r.direction(), r.direction());
-	auto half_b = dot(oc, r.direction());
-	auto c = dot(oc, oc) - radius*radius;
-	auto discriminant = half_b*half_b - 4*a*c;
-
-	if (discriminant < 0)
-		return -1.0;
-	else
-		return (-half_b - sqrt(discriminant)) / a;
-
-}
-
-
-color rayColor(const ray& r)
-{
-	auto t = hit_sphere(point3(0,0, -1), 0.5, r);
-	if (t > 0.0){
-		vec3 normal = unit_vector(r.at(t) - vec3(0, 0, -1));
-		return 0.5*color(normal.x()+1, normal.y()+1, normal.z()+1);
-	}
+	hit_record rec;
+	if (world.hit(r, 0, infinity, rec))
+			return 0.5 * (rec.normal + color(1, 1, 1));
 
 	vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5* (unit_direction.y() + 1.0);
@@ -42,6 +27,13 @@ int main() {
     size_t image_width = 400;
 		size_t image_height = static_cast<size_t>(image_width / aspect_ratio);
 		image_height = (image_height < 1) ? 1 : image_height;
+
+
+		// World
+		hittable_list world;
+
+		world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+		world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
 		// Camera
 
@@ -73,7 +65,7 @@ int main() {
 				auto ray_direction = pixel_center - camera_center;
 				ray r(camera_center, ray_direction);
 
-				color pixel_color = rayColor(r);
+				color pixel_color = rayColor(r, world);
 				writeColor(std::cout, pixel_color);
 			}
     }
